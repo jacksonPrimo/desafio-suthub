44ff0c12b3010de17af94fd196159a2b799f8b92
jacksonPrimo <jackson.primo12@gmail.com>
2021-12-24 09:43:15 -0300

    added page to list countries

---
+++ components/CountryCard.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-card class="card mx-1 my-1" role="button" :to="'/countries/'+country.cca2">
+    <v-img 
+      :src="country.flags.svg" 
+      height="200px"
+      class="white--text align-end"
+      gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+    >
+      <v-card-title class="card-title"> {{ country.name.common }} </v-card-title>
+    </v-img>
+  </v-card>
+</template>
+<script lang="ts">
+import Vue from "vue";
+import { CountryInterface } from "@/interfaces/countryInterface";
+export default Vue.extend({
+  props: {
+    country: {
+      type: Object as () => CountryInterface,
+      default: {} as CountryInterface
+    },
+  },
+  data: () => ({
+    show: false,
+  }),
+});
+</script>
+<style lang="scss">
+.card {
+  position: relative;
+}
+.card-title {
+  visibility: hidden;
+  position: absolute;
+  bottom: 0; 
+}
+.card:hover .card-title {
+  animation: showTitle 700ms ease-in-out forwards;
+}
+@keyframes showTitle {
+  0% {
+    visibility: hidden;
+    transform: translateY(45px);
+  }
+  100% {
+    visibility: visible;
+    transform: translateY(0px);
+  }
+}
+</style>
---
+++ pages/countries/index.vue
@@ -0,0 +1,129 @@
+<template>
+  <div class="d-flex flex-column">
+    <v-row class="mx-2">
+      <v-col lg="4">
+        <v-autocomplete
+          v-model="searchType"
+          :items="searchTypes"
+          label="Buscar por"
+        ></v-autocomplete>
+      </v-col>
+      <v-col lg="4">
+        <v-autocomplete
+          v-model="searchValue"
+          :items="searchOptions"
+          label="Valor"
+        ></v-autocomplete>
+      </v-col>
+      <v-col lg="4" class="d-flex align-items-center">
+        <v-btn color="primary" class="my-auto" @click="search()">
+          <v-icon>mdi-map-search</v-icon>
+        </v-btn>
+      </v-col>
+    </v-row>
+    <v-container fluid>
+      <v-row>
+        <v-col
+          v-for="(country, index) in paginedCountries"
+          :key="index"
+          cols="12"
+          lg="3"
+          md="4"
+        >
+          <CountryCard :country="country" />
+        </v-col>
+      </v-row>
+    </v-container>
+    <div class="text-center">
+      <v-pagination
+        v-model="page"
+        :length="pages"
+        :total-visible="6"
+        circle
+      ></v-pagination>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import CountryCard from "@/components/CountryCard.vue";
+import { CountryInterface } from "@/interfaces/countryInterface";
+export default Vue.extend({
+  name: 'IndexPage',
+  components: { CountryCard },
+  data: () => ({
+    page: 1,
+    itemsPerPage: 8,
+    allCountries: [],
+    filteredCountries: [],
+    searchTypes: [ "Nome", "Região", "Idioma" ],
+    searchType: "",
+    searchValue: ""
+  }),
+  computed: {
+    searchOptions() {
+      if(this.searchType === "Nome"){
+        return this.allCountries.map((ac: CountryInterface)=>{
+          return ac.name.common
+        })
+      } 
+      else if(this.searchType === "Região"){
+        return [...new Set(this.allCountries.map((ac: CountryInterface)=>{
+          return ac.region
+        }))]
+      }
+      else if(this.searchType === "Idioma"){
+        const langsByCountry = this.allCountries.map((ac: CountryInterface)=>{
+          if(ac.languages){
+            return Object.values(ac.languages)
+          } else {
+            return []
+          }
+        })
+        const concatened = langsByCountry.flat(1)
+        const withoutRepeat = [...new Set(concatened)]
+        return withoutRepeat;
+      }
+      else {
+        return []
+      }
+    },
+    pages(): number {
+      return Math.ceil(this.filteredCountries.length / this.itemsPerPage)
+    },
+    paginedCountries(): Array<CountryInterface> {
+      const start = (this.page - 1) * this.itemsPerPage;
+      const end = start + this.itemsPerPage;
+      return this.filteredCountries.slice(start, end);
+    },
+  },
+  mounted() {
+    this.$axios.$get("/all").then((resp) => {
+      this.allCountries = resp;
+      this.filteredCountries = resp;
+      console.log(resp.find(r=>{
+        if(r.capital){
+          return r.capital.length > 1
+        } else return false
+      }))
+    });
+  },
+  methods: {
+    search(){
+      this.page = 1
+      const type = 
+        this.searchType === 'Nome' ? 'name': 
+        this.searchType === 'Idioma' ? 'lang':
+        this.searchType === 'Região' ? 'region': null;
+      if(type && this.searchValue){
+        this.$axios.$get(`/${type}/${this.searchValue}`).then((resp) => {
+          this.filteredCountries = resp;
+        })
+      } else {
+        this.filteredCountries = this.allCountries
+      }
+    },
+  }
+})
+</script>
