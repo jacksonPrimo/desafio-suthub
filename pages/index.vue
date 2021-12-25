437798eb5cd013704e3bc96022b8528f3ee7da82
jacksonPrimo <jackson.primo12@gmail.com>
2021-12-25 11:07:52 -0300

    added form to register user

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,333 @@
 <template>
-  <div>hello word</div>
-</template>
+  <v-card class="mx-5 my-5 px-4 py-2">
+    <v-form
+      ref="form"
+      lazy-validation
+    > 
+      <v-row>
+        <v-col>
+        <v-text-field
+          v-model="formData.name"
+          :rules="nameRules"
+          label="Name"
+        ></v-text-field>
 
+        </v-col>
+        <v-col>
+          <v-text-field
+            v-model="formData.cpf"
+             v-mask="'###.###.###-##'"
+            :rules="cpfRules"
+            label="Cpf"
+          />
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-select
+            v-model="formData.petSpecie"
+            label="Espécie"
+            :items="['cão', 'gato', 'outro']"
+            :rules="petSpecieRules"
+            @change="formData.petBreed = ''"
+          ></v-select>
+        </v-col>
+        <v-col>
+          <v-text-field
+            v-if="formData.petSpecie === 'outro'"
+            v-model="formData.petBreed"
+            label="Raça"
+            :rules="petBreedRules"
+          ></v-text-field>
+          <v-select
+            v-else
+            v-model="formData.petBreed"
+            label="Raça"
+            :items="breeds"
+            :rules="petBreedRules"
+          ></v-select>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+        <v-text-field
+          v-model="formData.birthDate"
+          label="Data de Nascimento"
+          :rules="birthDateRules"
+          type="datetime-local"
+        ></v-text-field>
+
+        </v-col>
+        <v-col>
+          <v-text-field
+            v-model="formData.monthlyIncome"
+            type="number"
+            min="0"
+            label="Renda Mensal"
+            :rules="monthlyIncomeRules"
+          />
+        </v-col>
+      </v-row>
+      <div>
+        Dados de Endereço
+        <v-row>
+          <v-col>
+            <v-text-field
+              v-model="formData.address.cep"
+              v-mask="'#####-###'"
+              label="Cep"
+              :rules="cepRules"
+              @change="autoCompleteAddress()"
+            ></v-text-field>
+          </v-col>
+          <v-col>
+            <v-text-field
+              v-model="formData.address.city"
+              label="Cidade"
+              :rules="cityRules"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col lg="4">
+            <v-select
+              v-model="formData.address.state"
+              label="Estado"
+              :items="states"
+              :rules="stateRules"
+            ></v-select>
+          </v-col>
+          <v-col lg="4">
+            <v-text-field
+              v-model="formData.address.district"
+              label="Bairro"
+              :rules="districtRules"
+            ></v-text-field>
+          </v-col>
+          <v-col lg="4">
+            <v-text-field
+              v-model="formData.address.street"
+              label="Rua"
+              :rules="streetRules"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+      </div>
+      <v-btn
+        color="success"
+        class="mr-4"
+        @click="register"
+      >
+        Registrar
+      </v-btn>
+    </v-form>
+  </v-card>
+</template>
 <script>
+import { required, minLength, minValue } from 'vuelidate/lib/validators'
+import { cpf } from 'cpf-cnpj-validator';
+import Swal from 'sweetAlert2'
 export default {
-  name: 'IndexPage',
+  name: "IndexPage",
+  validations: {
+    formData: {
+      name: {
+        required,
+        lastName: v => {
+          const splited = v.split(' ') 
+          return (splited.length > 1 && splited[1].length > 0)
+        }
+      },
+      cpf: {
+        required,
+        minLength: minLength(14),
+        valid: v => cpf.isValid(v)
+      },
+      petSpecie: {
+        required,
+      },
+      petBreed: {
+        required,
+      },
+      birthDate: {
+        required,
+        ageRange: v => {
+          const today = new Date();
+          const birthDate = new Date(v);
+          let age = today.getFullYear() - birthDate.getFullYear();
+          const m = today.getMonth() - birthDate.getMonth();
+          if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+            age--;
+          }
+          return (age >= 18 && age <= 65)
+        }
+      },
+      monthlyIncome: {
+        required,
+        minValue: minValue(1000)
+      },
+      address: {
+        cep: {
+          required,
+          minLength: minLength(9)
+        },
+        state: {
+          required
+        },
+        city: {
+          required
+        },
+        district: {
+          required
+        },
+        street: {
+          required
+        },
+      }
+    }
+  },
+  data: () => ({
+    formData: {
+      name: '',
+      cpf: '',
+      petSpecie: '',
+      petBreed: '',
+      birthDate: '',
+      monthlyIncome: 0,
+      address: {
+        cep: '',
+        state: '',
+        city: '',
+        district: '',
+        street: '',
+      }
+    },
+    states: ["AC", "AL", "AM", "AP", "BA", "CE", "DF", "ES", "GO", "MA", "MG", "MS", "MT", "PA", "PB", "PE", "PI", "PR", "RJ", "RN", "RO", "RR", "RS", "SC", "SE", "SP", "TO"]
+  }),
+  computed: {
+    nameRules() {
+      return [
+        () => this.$v.formData.name.required || 'O campo nome é obrigatório',
+        () => this.$v.formData.name.lastName || 'Preencha com nome e sobrenome'
+      ]
+    },
+    cpfRules(){
+      return [
+        () => this.$v.formData.cpf.required || 'Cpf é um campo obrigatório',
+        () => this.$v.formData.cpf.minLength || 'Preencha com os 11 caracteres',
+        () => this.$v.formData.cpf.valid || 'Preencha com um cpf válido'
+      ]
+    },
+    petSpecieRules() {
+      return [
+        () => this.$v.formData.petSpecie.required || 'Este campo é obrigatório',
+      ]
+    },
+    petBreedRules() {
+      return [
+        () => this.$v.formData.petBreed.required || 'Este campo é obrigatório',
+      ]
+    },
+    birthDateRules() {
+      return [
+        () => this.$v.formData.birthDate.required || 'Este campo é obrigatório',
+        () => this.$v.formData.birthDate.ageRange || 'A idade permitida é entre 18 e 65 anos',
+      ]
+    },
+    monthlyIncomeRules() {
+      return [
+        () => this.$v.formData.monthlyIncome.required || 'Este campo é obrigatório',
+        () => this.$v.formData.monthlyIncome.minValue || 'Valor mínimo de R$ 1000,00',
+      ]
+    },
+    cepRules() {
+      return [
+        () => this.$v.formData.address.cep.required || 'Este campo é obrigatório',
+        () => this.$v.formData.address.cep.minLength || 'Preencha com os 8 caracteres',
+      ]
+    },
+    stateRules() {
+      return [
+        () => this.$v.formData.address.state.required || 'Este campo é obrigatório',
+      ]
+    },
+    cityRules() {
+      return [
+        () => this.$v.formData.address.city.required || 'Este campo é obrigatório',
+      ]
+    },
+    districtRules() {
+      return [
+        () => this.$v.formData.address.district.required || 'Este campo é obrigatório',
+      ]
+    },
+    streetRules() {
+      return [
+        () => this.$v.formData.address.street.required || 'Este campo é obrigatório',
+      ]
+    },
+    breeds(){
+      switch(this.formData.petSpecie){
+        case 'cão':
+          return ['Pastor Alemão', 'Pug', 'Poodle', 'Vira Lata', 'buldogue'];
+        case 'gato':
+          return ['Persa', 'Siamês', 'Ragdoll', 'Angorá', 'Thai'];
+        default: 
+          return []
+      }
+    }
+  },
+  methods: {
+    async autoCompleteAddress(){
+      if(!this.$v.formData.address.cep.$invalid){
+        const proceed = await Swal.fire({
+          title: 'Deseja auto preencher os outros campos?',
+          showCancelButton: true,
+          confirmButtonText: `Sim`,
+          cancelButtonText: `Não`
+        })
+        if(proceed.isConfirmed) {
+          try {
+            const { data } = await this.$axios.get(`${process.env.API_CEP}${this.formData.address.cep}/json`)
+            if(data.erro){
+              Swal.fire('Desculpe não encontramos endereço com este cep', '', 'error')
+              return
+            }
+            this.formData.address.state = data.uf
+            this.formData.address.city = data.localidade
+            this.formData.address.district = data.bairro
+            this.formData.address.street = data.logradouro
+          } catch(error){
+            Swal.fire('Desculpe ocorreu um erro inesperado em nossa busca', '', 'error')
+          }
+        }
+      }
+    },
+    register(){
+      if(this.$v.$invalid){
+        Swal.fire('Preencha todos os dados corretamente', '', 'error')
+        this.$refs.form.validate()
+      } else {
+        this.registerInBackend(this.formData.name)
+          .then(()=>{
+            Swal.fire('Usuário cadastrado com sucesso', '', 'success')
+          })
+          .catch(()=>{
+            Swal.fire('Erro ao cadastrar usuário', '', 'error')
+          })
+      }
+    },
+    // apenas uma simulação de requisição a uma api
+    registerInBackend(data){
+      console.log(data)
+      return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+          Math.random() > 0.5 
+            ? resolve({code: 200, message: 'success in register'}) 
+            : reject(new Error('error on register'))
+        }, 3000)
+      })
+    }
+  },
 }
 </script>
