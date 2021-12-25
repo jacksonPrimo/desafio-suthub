ec90e7fe177ee820b96b58e59e2643ed1a1d83f4
jacksonPrimo <jackson.primo12@gmail.com>
2021-12-25 17:41:45 -0300

    added config to deploy app in netlify correctly

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,12 +30,10 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    // https://go.nuxtjs.dev/typescript
     '@nuxt/typescript-build',
-    // https://go.nuxtjs.dev/stylelint
     '@nuxtjs/stylelint-module',
-    // https://go.nuxtjs.dev/vuetify
     '@nuxtjs/vuetify',
+    '@nuxtjs/netlify-files',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
