a4a27c0e67731ac9ef88b8906b8e50ec0f681032
jacksonPrimo <jackson.primo12@gmail.com>
2021-12-25 17:04:40 -0300

    adjusted ssr in nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,8 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
+  target: 'static',
+  ssr: true,
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - desafio-suthub',
