42fddb41d7c28f18bc830f23b1221230181c6ed1
jacksonPrimo <jackson.primo12@gmail.com>
2021-12-28 20:54:55 -0300

    adjusted pwa

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,15 +5,15 @@ export default {
   ssr: true,
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - desafio-suthub',
-    title: 'desafio-suthub',
+    titleTemplate: 'Desafio Suthub',
+    title: 'Desafio Suthub',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/suthub.ico' }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
@@ -52,6 +52,14 @@ export default {
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
+    meta: {
+      title: 'suthub',
+      author: 'jackson aquino primo'
+    },
+    icon: {
+      fileName: 'icon.png',
+      sizes: [64, 120, 144, 152, 192, 384, 512]
+    },
     manifest: {
       name: 'Desafio Suthub',
       lang: 'pt-br',
