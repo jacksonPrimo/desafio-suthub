27f57a1c336e7c4d7e42ca9ef5ad0ffb8c061786
jacksonPrimo <jackson.primo12@gmail.com>
2021-12-28 13:19:45 -0300

    adjusted colors and logo in header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,28 +1,29 @@
 <template>
   <v-app>
-    <v-app-bar app dark color="blue-grey darken-4">
-      <div>
-        <v-toolbar-title> SUTHUB </v-toolbar-title>
-      </div>
-      <v-icon>mdi-earth</v-icon>
-      <v-tabs>
-        <v-tab to="/countries">
-          <v-icon>mdi-map-search-outline</v-icon>
-          PAÍSES
-        </v-tab>
+    <v-app-bar app light>
+      <v-img 
+        class="mr-3" 
+        src="/logo-suthub.png" 
+        width="150px"
+      ></v-img>
+      <v-tabs color="teal draken-3">
         <v-tab to="/">
           <v-icon>mdi-account-plus</v-icon>
           CADASTRO
         </v-tab>
+        <v-tab to="/countries">
+          <v-icon>mdi-map-search-outline</v-icon>
+          PAÍSES
+        </v-tab>
       </v-tabs>
     </v-app-bar>
 
-    <v-main>
+    <v-main class="my-4">
       <Nuxt />
     </v-main>
-    <v-footer class="pa-0" dark color="blue-grey darken-4">
-      <v-card flat tile width="100%" class="lighten-1 text-center">
-        <v-card-text class="white--text">
+    <v-footer class="pa-0" dark>
+      <v-card flat tile width="100%" class="text-center" color="teal">
+        <v-card-text class="gray--text">
           {{ new Date().getFullYear() }} — <strong>Desafio Suthub</strong>
         </v-card-text>
       </v-card>
